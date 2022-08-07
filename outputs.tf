6e1bcd19b7a32d57303cb80c0d6231de84638b29
Diego Lambrechts <diego.lambrechts@gmail.com>
2022-08-07 00:45:58 -0300

    esf

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "id" {
   description = "List of IDs of instances"
-  value       = ["${module.ec2.id}"]
+  value       = ["${module.ec2_k3s_cp.id}"]
 }
 
 output "public_dns" {
   description = "List of public DNS names assigned to the instances"
-  value       = ["${module.ec2.public_dns}"]
+  value       = ["${module.ec2_k3s_cp.public_dns}"]
 }
 
 output "instance_id" {
   description = "EC2 instance ID"
-  value       = "${module.ec2[0]}"
+  value       = "${module.ec2_k3s_cp[0]}"
 }
