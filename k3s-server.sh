e78cdb5a0667562e677843d8c3e9dee760cfe220
Diego Lambrechts <diego.lambrechts@gmail.com>
2022-08-07 20:58:34 -0300

    dwa

--- k3s-server.sh
+++ k3s-server.sh
@@ -1,4 +1,6 @@
- #! /bin/bash
- yum update -y
- curl -sfL https://get.k3s.io | sh -
- cp /etc/rancher/k3s/k3s.yaml ~/ec2-user/.kube/config
+#! /bin/bash
+yum update -y
+curl -sfL https://get.k3s.io | sh -
+mkdir .kube
+cp /etc/rancher/k3s/k3s.yaml ~/.kube/config
+echo "export KUBECONFIG=\"$HOME/.kube/config\"" >> .bashrc
--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 /* output "id" {
   description = "List of IDs of instances"
   value       = ["${module.ec2_k3s_cp.id}"]
-}
+} */
 
 output "public_dns" {
   description = "List of public DNS names assigned to the instances"
-  value       = ["${module.ec2_k3s_cp.public_dns}"]
+  value       = ["${module.ec2_k3s-main.public_dns}"]
 }
 
-output "instance_id" {
+/* output "instance_id" {
   description = "EC2 instance ID"
   value       = "${module.ec2_k3s_cp[0]}"
 } */
