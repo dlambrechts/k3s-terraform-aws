cb4364f58330a56f07ffd395278e8dae223666d5
Diego Lambrechts <diego.lambrechts@gmail.com>
2022-08-07 21:49:09 -0300

    daw

--- k3s-server.sh
+++ k3s-server.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 yum update -y
+
 curl -sfL https://get.k3s.io | sh -
+
 mkdir /home/ec2-user/.kube
 cp /etc/rancher/k3s/k3s.yaml /home/ec2-user/.kube/config
+chown ec2-user /home/ec2-user/.kube
+chmod 644 /home/ec2-user/.kube
+
 echo "export KUBECONFIG=\"/home/ec2-user/.kube/config\"" >> /home/ec2-user/.bashrc
