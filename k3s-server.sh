1c7bab5162001b509143f9dc2e0b1de72bfd3d44
Diego Lambrechts <diego.lambrechts@gmail.com>
2022-08-15 21:34:37 -0300

    fixed script for tls-san

--- k3s-server.sh
+++ k3s-server.sh
@@ -3,7 +3,7 @@ yum update -y
 
 public_ip=$(curl http://169.254.169.254/latest/meta-data/public-ipv4)
 
-curl -sfL https://get.k3s.io | sh - server --tls-san $public_ip
+curl -sfL https://get.k3s.io | sh -s - --tls-san $public_ip
 
 mkdir /home/ec2-user/.kube
 cp /etc/rancher/k3s/k3s.yaml /home/ec2-user/.kube/config
